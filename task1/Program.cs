0f4044cf154970b20ab4b68d375d0ea9ac1c687e
Maria Popova <marypoppens14@gmail.com>
2023-05-03 20:45:54 +0300

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// принимает на вход пятизначное число 
+// проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число: ");
+string a = Console.ReadLine();
+int length = a.Length;
+
+if (length == 5)
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine($"{a} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{a} - не палиндром");
+    }
+}
+
+else
+{
+    Console.WriteLine("Число не является пятизначным");
+}
