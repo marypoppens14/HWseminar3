19fc4cc9c05873b97ffff6b90aac70b1737f7ee4
Maria Popova <marypoppens14@gmail.com>
2023-05-03 20:47:12 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход число (N) 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int x = 1; x <= a; x++)
+{
+    Console.WriteLine($"{x * x * x}");
+}
